93d77ca5b3f7816f3c28efb6ebff48593411516b
Dacamo <castrom1998@gmail.com>
2020-03-26 18:47:58 -0500

    Added ManejadorErrores

---
+++ Compilador/ManejadorErrores/GestorErrores.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Compilador.ManejadorErrores
+{
+    public class GestorErrores
+    {
+        private static Dictionary<TipoError, List<???>> errores;
+
+    }
+}
---
+++ Compilador/ManejadorErrores/TipoError.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Compilador.ManejadorErrores
+{
+    public enum TipoError
+    {
+        LEXICO, SINTATICO, SEMANTICO
+    }
+}
