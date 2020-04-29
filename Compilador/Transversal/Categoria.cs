b277acd48ab5974678423e24a6f3c189e8e9acaa
Dacamo <castrom1998@gmail.com>
2020-04-29 17:33:08 -0500

    Adding transversal folder

--- Compilador/Transversal/Categoria.cs
+++ Compilador/Transversal/Categoria.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace Compilador.Transversal
+﻿namespace Compilador.Transversal
 {
     public enum Categoria
     {
--- Compilador/Transversal/ComponenteLexico.cs
+++ Compilador/Transversal/ComponenteLexico.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Text;
 
 namespace Compilador.Transversal
 {
@@ -11,20 +9,44 @@ namespace Compilador.Transversal
         public int NumeroLinea { get; set; }
         public int PosicionInicial { get; set; }
         public int PosicionFinal { get; set; }
-        public TipoComponente Tipo { get; set;  }
+        public TipoComponente Tipo { get; set; }
 
+        public static ComponenteLexico crear(string lexema, Categoria categoria, int numeroLinea, int posicionInicial, int posicionFinal)
+        {
+            ComponenteLexico retorno = new ComponenteLexico();
+            retorno.Lexema = lexema;
+            retorno.Categoria = categoria;
+            retorno.NumeroLinea = numeroLinea;
+            retorno.PosicionFinal = posicionFinal;
+            retorno.PosicionInicial = posicionInicial;
+
+            return retorno;
+        }
+
+        //para las palabras reservadas
+        public static ComponenteLexico crear(string lexema, Categoria categoria)
+        {
+            ComponenteLexico retorno = new ComponenteLexico();
+            retorno.Lexema = lexema;
+            retorno.Categoria = categoria;
+            retorno.NumeroLinea = 0;
+            retorno.PosicionFinal = 0;
+            retorno.PosicionInicial = 0;
+
+            return retorno;
+        }
         public override string ToString()
         {
             StringBuilder retorno = new StringBuilder();
-            
+
             retorno.Append("Tipo componente: " + Tipo.ToString() + "\n");
             retorno.Append("Categoria: " + Categoria + "\n");
             retorno.Append("Lexema: " + Lexema + "\n");
             retorno.Append("Numero de Linea: " + NumeroLinea + "\n");
             retorno.Append("Posicion Inicial: " + PosicionInicial + "\n");
             retorno.Append("Poscicion Final: " + PosicionFinal + "\n");
-           
-            return base.ToString();
+
+            return retorno.ToString();
         }
     }
 }
